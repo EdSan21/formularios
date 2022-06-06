969ebd7c539e1492a5dc1c797a82e9e303102330
Edvin Perez <edvinperez237@gmail.com>
2022-06-06 08:23:47 -0600

    Tarea: Formulario

---
+++ src/app/componentes/form-class/form-class.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-class',
+  templateUrl: './form-class.component.html',
+  styleUrls: ['./form-class.component.css']
+})
+export class FormClassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
